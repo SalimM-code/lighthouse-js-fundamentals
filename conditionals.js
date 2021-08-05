5513b65309091706d9c86b844fe2052683b79b6a
Salim Mustafa <mustafasalim000@gmail.com>
2021-08-05 02:01:58 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -26,3 +26,11 @@ if (temperature < 0) {
 
 console.log("Now you're ready to go outside!")
 
+// logical operators
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
+}
