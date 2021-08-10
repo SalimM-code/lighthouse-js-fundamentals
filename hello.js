4647a8e0654846511597c6695ef292bf24a755d6
Salim Mustafa <mustafasalim000@gmail.com>
2021-08-10 01:20:51 +0000

    using a function to print a greet.

---
+++ hello.js
@@ -0,0 +1,13 @@
+// const sayHello = function () {
+//     console.log("Hello, world");
+// }
+
+// sayHello();
+
+// make the function more useful
+
+const sayHello = function (name) {
+    console.log("Hello, " + name);
+}
+
+sayHello("salim");
