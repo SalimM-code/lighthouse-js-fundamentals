43d9fdbcaae75dfbd52ee918564731a1869a5141
Salim Mustafa <mustafasalim000@gmail.com>
2021-08-13 16:42:17 +0000

    changed the loop from for loop to while loop.

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,9 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-for (let i = 0; i < packingList.length; i++){
+let i = 0;
+while (i < packingList.length) {
     console.log(packingList[i]);
-}
+    i++
+};
 
 // this code will log all the items in the packingList
